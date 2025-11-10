f7cdd60e0e2d5ce90bb57d0583cd404107354541
Eno <enoyoo02@gmail.com>
2025-11-10 13:02:57 -0500

    location search debouncing

---
+++ LocationAndPlaceLookup/PlaceLookupView.swift
@@ -0,0 +1,18 @@
+//
+//  PlaceLookupView.swift
+//  LocationAndPlaceLookup
+//
+//  Created by Eno Yoo on 11/10/25.
+//
+
+import SwiftUI
+
+struct PlaceLookupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlaceLookupView()
+}
---
+++ LocationAndPlaceLookup/PlaceLookupViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PlaceLookupViewModel.swift
+//  LocationAndPlaceLookup
+//
+//  Created by Eno Yoo on 11/10/25.
+//
+
+import Foundation
