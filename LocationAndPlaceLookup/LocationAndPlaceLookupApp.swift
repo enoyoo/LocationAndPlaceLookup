a0758de345c8230277b9771c31a85718c49c2269
Eno <enoyoo02@gmail.com>
2025-11-10 10:57:36 -0500

    Initial Commit

---
+++ LocationAndPlaceLookup/LocationAndPlaceLookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationAndPlaceLookupApp.swift
+//  LocationAndPlaceLookup
+//
+//  Created by Eno Yoo on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct LocationAndPlaceLookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
